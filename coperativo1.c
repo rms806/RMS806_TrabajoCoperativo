a9e6bc726402c25b6e02913b6e5f1e7bef5a50af
rms806 <126265690+rms806@users.noreply.github.com>
2023-06-20 12:01:41 +0200

    Create coperativo1.c

---
+++ coperativo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* Programa inicial */
+
+int main(){
+    printf("Aquí estoy de nuevo.\n");
+    printf("Este es mi segundo programa de la asignatura.\n");
+    printf("Me he limitado a cambiar el texto de una orden de escritura.\n");
+    printf("\n\n\nPulse una tecla para finalizar");
+    getchar();
+    return 0;
+}
